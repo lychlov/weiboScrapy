24d92e6b864012f072800440ebc85daddae25ffc
lychlov <czk499658904@126.com>
2018-04-24 09:25:15 +0800

    mongodb 存储tweet完成，retweet待配置

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 微博爬虫
+## 概要设计
+https://shimo.im/docs/jzzS9Zp4vS8DL6iq
+## 需求
+- 根据关键词组和目标微博ID，爬取微博博文、评论等内容
+- 将爬取结果保存到MongoDB
+- 爬虫可持续运行，增量爬取内容
+## 主要模块
+- 模拟登录\login：
+- IP代理\proxy:
+
+- 爬虫\spiders:
+    - tweets：
+    - tweets_to_id
+    - comments：
+- ITEM\items
+    - TweetItem:
+    - UserItem:
+    - CommentItem:
+    - CommentUserItem:
+- Pipeline\pipelines:
+- 任务调度\scheduler:
+
--- weiboScrapy/settings.py
+++ weiboScrapy/settings.py
@@ -24,12 +24,13 @@ MONGO_DATABASE = 'test'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0
+DOWNLOAD_DELAY = 2
+#DOWNLOAD_TIMEOUT=6
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -57,7 +58,7 @@ COOKIES_ENABLED = False
 DOWNLOADER_MIDDLEWARES = {
     'weiboScrapy.middlewares.UserAgentDownloaderMiddleware': 543,
     'weiboScrapy.middlewares.WeiboscrapyDownloaderMiddleware': 143,
-    'weiboScrapy.middlewares.ProxyDownloaderMiddleware': 243,
+    #'weiboScrapy.middlewares.ProxyDownloaderMiddleware': 243,
 
 }
 
